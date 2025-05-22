9d81e90eac4305049db3783e5fa3afcd81deb451
William Pearce <williampearcebusiness@gmail.com>
2025-05-22 23:05:58 +0100

    init: Add files developed and created previously
    Added all of the files that I have worked on previously in this
    project. These are:
    
    new file:   css/_base.scss
    new file:   css/_variables.scss
    new file:   css/style.css
    new file:   css/style.css.map
    new file:   css/style.scss
    new file:   img/services-graphic.png
    new file:   img/william-headshot.jpg
    new file:   img/william-photo.jpg
    new file:   img/williampearcedev-logo.png
    new file:   img/wp-logo.png
    new file:   index.html
    new file:   pages/contact.html
    new file:   pages/projects.html
    new file:   pages/services.html
    new file:   script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Ensure infinite scrolling with no gaps
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollContent = document.querySelector('#scroll-section .scroll-content');
+
+  // Duplicate items to create seamless scrolling
+  scrollContent.innerHTML += scrollContent.innerHTML;
+
+  // Set the width of the scroll-content to fit all items (ensures no gaps)
+  const scrollItems = document.querySelectorAll('#scroll-section .scroll-item');
+  const totalWidth = Array.from(scrollItems).reduce((sum, item) => {
+      return sum + item.offsetWidth + parseInt(window.getComputedStyle(item).marginRight || 0);
+  }, 0);
+  scrollContent.style.width = `${totalWidth}px`;
+});
